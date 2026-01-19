753a6ce8e99792b0da395bc9dd5c023d2d6f3758
Wannawatkul <Wannawatkul@gmail.com>
2026-01-19 19:11:20 +0700

    Updates

--- _next/static/-OsbsdLTiW-Vq8riv2SSW/_buildManifest.js
+++ _next/static/-OsbsdLTiW-Vq8riv2SSW/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/Profile-managemen//_next/:path+",
+        "destination": "/Profile-managemen/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
